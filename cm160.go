4dbd6ac595082d3f0e7a897e3971cbe778af9254
Tim Retout <tim@retout.co.uk>
2015-09-26 23:12:56 +0100

    Add Record struct representing data to be read

--- cm160.go
+++ cm160.go
@@ -5,3 +5,19 @@ import "github.com/kylelemons/gousb/usb"
 type CM160 struct {
 	device *usb.Device
 }
+
+type Record struct {
+	Volt   int
+	Year   int
+	Month  int
+	Day    int
+	Hour   int
+	Minute int
+	Cost   float32
+	Amps   float32
+	IsLive bool
+	Watt   float32
+}
+
+func Register(cb func(*Record)) {
+}

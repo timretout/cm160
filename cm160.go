cd7c390f570f36f2cf504121b7f392f5a5144683
Tim Retout <tim@retout.co.uk>
2015-09-27 23:05:08 +0100

    Open correct device, with lots of debugging

--- cm160.go
+++ cm160.go
@@ -1,6 +1,10 @@
 package cm160
 
-import "github.com/kylelemons/gousb/usb"
+import (
+	"log"
+
+	"github.com/kylelemons/gousb/usb"
+)
 
 type CM160 struct {
 	device *usb.Device
@@ -19,5 +23,40 @@ type Record struct {
 	Watt   float32
 }
 
-func Register(cb func(*Record)) {
+var ctx *usb.Context
+
+func init() {
+	ctx = usb.NewContext()
+}
+
+const (
+	CM160_VENDOR  = 0x0fde
+	CM160_PRODUCT = 0xca05
+)
+
+func isCM160(desc *usb.Descriptor) bool {
+	return desc.Vendor == usb.ID(CM160_VENDOR) &&
+		desc.Product == usb.ID(CM160_PRODUCT)
+}
+
+func Open() {
+	log.Println("Starting!")
+
+	devices, err := ctx.ListDevices(isCM160)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println(devices)
+
+	for _, v := range devices {
+		log.Println("device: ", v)
+		v.Close()
+	}
+
+	log.Println("Ending!")
+}
+
+func (c *CM160) Register(cb func(*Record)) {
+
 }
